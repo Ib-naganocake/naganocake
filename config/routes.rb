31f154ce5b532c1e36cdc34b6e1c52bda3065a8d
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-08-17 02:22:16 +0000

    Add MVC

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,35 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
+  scope module: :public do
+    root to: "homes#top"
+    get "about" => "homes#about"
+    resources :items, only: [:index, :show]
+    resource :customers, only: [:update] do
+      get "my_page" => "customers#show"
+      get "information/edit" => "customers#edit"
+      get "confirm_withdraw"
+      patch "withdraw"
+    end
+    resources :cart_items, only: [:index, :update, :create, :destroy] do
+      delete "destroy_all", on: :collection
+    end
+    resources :orders, only: [:index, :show, :create] do
+      collection do
+        post :confirm
+        get :complete
+      end
+    end
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  end
+
+  namespace :admin do
+    get "top" => "homes#top"
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update] do
+      resources :order_details, only: [:update]
+    end
+  end
 end

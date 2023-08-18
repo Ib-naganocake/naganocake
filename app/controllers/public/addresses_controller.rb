31fdc50be288c22d3641f2148dc4698d4068f9df
yuuki4682 <132807007+yuuki4682@users.noreply.github.com>
2023-08-18 03:03:51 +0000

    add address_view

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,5 +1,19 @@
 class Public::AddressesController < ApplicationController
   def index
+    @addresses = Address.all
+    
+    @address = Address.new
+  end
+  
+  def create
+    @address = Address.new
+    @address.customer_id = current_user.id
+    if @address.save
+      redirect_to request.referer
+    else
+      @addresses = Address.all
+      render index
+    end
   end
 
   def edit

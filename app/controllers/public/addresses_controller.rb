47905ac11aa3b7857c0f5c643d9ce3a6c60ecc5a
yuuki4682 <132807007+yuuki4682@users.noreply.github.com>
2023-08-18 05:36:29 +0000

    add addresses

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -6,16 +6,38 @@ class Public::AddressesController < ApplicationController
   end
   
   def create
-    @address = Address.new
-    @address.customer_id = current_user.id
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
     if @address.save
       redirect_to request.referer
     else
       @addresses = Address.all
-      render index
+      render :index
     end
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
+  
+  def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to addresses_path
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    address = Address.find(params[:id])
+    address.destroy
+    redirect_to request.referer
+  end
+  
+  private
+  def address_params
+    params.required(:address).permit(:postal_code, :address, :name)
+  end
+  
 end

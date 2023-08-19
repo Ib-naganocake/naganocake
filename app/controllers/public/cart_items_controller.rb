cf4c48007f7594da5eec56c31eaffba05af624c3
yuuki4682 <132807007+yuuki4682@users.noreply.github.com>
2023-08-19 07:17:36 +0000

    pull admin_item_index

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,21 @@
 class Public::CartItemsController < ApplicationController
+  before_action :authenticate_customer!
+  
   def index
   end
+  
+  def create
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    if @cart_item.save
+      redirect_to cart_items_path
+    else
+      redirect_to item_path(@cart_item.item)
+    end
+  end
+  
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
+  end
+  
 end

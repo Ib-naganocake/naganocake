03303abbb02f67222158c13b90bebab1b50e49fc
yuuki4682 <132807007+yuuki4682@users.noreply.github.com>
2023-08-19 01:02:50 +0000

    pull add_item

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,13 +9,13 @@ class Item < ApplicationRecord
   validates :price_without_tax, presence:true
   
   # 商品画像のサイズ調整
-  def get_imagee(weight, height)
+  def get_image(width, height)
     unless image.attached?
       # 商品画像がない場合はimages/no-image.jpgを参照
-      file_path = Rails.root.join('app/assets/images/no-image.jpg')
-      イメージ名.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-  image.variant(resize_to_limit: [weight, height]).processed
+  image.variant(resize_to_limit: [width, height]).processed
   end
   
 end

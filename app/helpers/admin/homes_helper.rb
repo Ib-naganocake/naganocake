c2c651a71a1e212ced4d492c19eb94a15e7fa373
ryumasgit <135499359+ryumasgit@users.noreply.github.com>
2023-08-20 02:47:10 +0000

    Add admin-orders

--- app/helpers/admin/homes_helper.rb
+++ app/helpers/admin/homes_helper.rb
@@ -2,8 +2,8 @@ module Admin::HomesHelper
   
   def calculate_order_total_amount(order)
     total_amount = 0
-    order.order_datails.each do |order_datail|
-      total_amount += order_datail.amount
+    order.order_details.each do |order_detail|
+      total_amount += order_detail.amount
     end
     total_amount
   end
--- app/helpers/admin/orders_helper.rb
+++ app/helpers/admin/orders_helper.rb
@@ -1,14 +1,19 @@
 module Admin::OrdersHelper
-  
+
   def calculate_order_total_amount(order)
     total_amount = 0
-    order.order_datails.each do |order_datail|
-      total_amount += order_datail.amount
+    order.order_details.each do |order_detail|
+      total_amount += order_detail.amount
     end
     total_amount
   end
-  
+
   def customer_full_name(order)
     "#{order.customer.family_name} #{order.customer.first_name}"
+  end
+    
+  def calculate_total_subtotal(order)
+    total_subtotal = 0
+    total_subtotal += order.order_details.
   end
 end

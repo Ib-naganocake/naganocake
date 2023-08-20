3d515a0224787893e92685e68b372dfd933440d0
yuuki4682 <132807007+yuuki4682@users.noreply.github.com>
2023-08-20 06:45:04 +0000

    pull cart edit

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -56,6 +56,7 @@ class Public::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @order_details = OrderDetail.where(order_id: @order.id)
   end
 
   private

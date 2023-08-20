0705156890586b29332f7159d66872eb8d87141a
yuuki4682 <132807007+yuuki4682@users.noreply.github.com>
2023-08-20 00:32:30 +0000

    add create_cart_item

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -6,10 +6,15 @@ class Public::CartItemsController < ApplicationController
   
   def create
     @cart_item = current_customer.cart_items.new(cart_item_params)
-    if @cart_item.save
-      redirect_to cart_items_path
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.save
+      redirect_to cart_items_path, notice: "商品を追加しました。"
+    elsif @cart_item.save
+      redirect_to cart_items_path, notice: "商品をカートに登録しました。"
     else
-      redirect_to item_path(@cart_item.item)
+      redirect_to item_path(@cart_item.item), notice: "個数を選択してください"
     end
   end
   

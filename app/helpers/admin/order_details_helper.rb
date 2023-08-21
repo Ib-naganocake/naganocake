3c213b95e3f47e35e9d9103a1e1180d2409cc8a1
ryumasgit <135499359+ryumasgit@users.noreply.github.com>
2023-08-21 05:44:53 +0000

    fix admin-orders

--- app/helpers/admin/order_details_helper.rb
+++ app/helpers/admin/order_details_helper.rb
@@ -5,18 +5,28 @@ module Admin::OrderDetailsHelper
     subtotal
   end
 
-  def transaction_status_options(order)
-    case order.transaction_status
-    when "waiting", "paid_up", "shipped"
-      options_for_transaction_status
+  def production_status_options(order_detail)
+    case order_detail.production_status
+    when "waiting"
+      options_for_waiting(order_detail)
+    when "producting"
+      options_for_producting(order_detail)
+    else
+      [[I18n.t("enums.order_detail.production_status.#{order_detail.production_status}"), order_detail.production_status]]
     end
   end
 
-  def options_for_transaction_status
+  def options_for_waiting(order_detail)
     [
-      [I18n.t("enums.order.transaction_status.pending"), "waiting"],
-      [I18n.t("enums.order.transaction_status.paid_up"), "paid_up"],
-      [I18n.t("enums.order.transaction_status.shipped"), "shipped"]
+      [I18n.t("enums.order_detail.production_status.#{order_detail.production_status}"), order_detail.production_status],
+      [I18n.t("enums.order_detail.production_status.producting"), "producting"]
+    ]
+  end
+
+  def options_for_producting(order_detail)
+    [
+      [I18n.t("enums.order_detail.production_status.#{order_detail.production_status}"), order_detail.production_status],
+      [I18n.t("enums.order_detail.production_status.complete"), "complete"]
     ]
   end
 end
--- app/helpers/admin/orders_helper.rb
+++ app/helpers/admin/orders_helper.rb
@@ -11,4 +11,29 @@ module Admin::OrdersHelper
   def customer_full_name(order)
     "#{order.customer.family_name} #{order.customer.first_name}"
   end
+
+  def transaction_status_options(order)
+    case order.transaction_status
+    when "waiting"
+      options_for_waiting(order)
+    when "preparing"
+      options_for_preparing(order)
+    else
+      [[I18n.t("enums.order.transaction_status.#{order.transaction_status}"), order.transaction_status]]
+    end
+  end
+
+  def options_for_waiting(order)
+    [
+      [I18n.t("enums.order.transaction_status.#{order.transaction_status}"), order.transaction_status],
+      [I18n.t("enums.order.transaction_status.paid_up"), "paid_up"]
+    ]
+  end
+
+  def options_for_preparing(order)
+    [
+      [I18n.t("enums.order.transaction_status.#{order.transaction_status}"), order.transaction_status],
+      [I18n.t("enums.order.transaction_status.shipped"), "shipped"]
+    ]
+  end
 end

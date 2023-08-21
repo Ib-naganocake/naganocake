28127bb0df1c127802a6fe10410bf6af3088baf9
ryumasgit <135499359+ryumasgit@users.noreply.github.com>
2023-08-21 04:26:52 +0000

    fix admin-orders

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -23,21 +23,6 @@ class Admin::OrdersController < ApplicationController
     end
   end
 
-  def transaction_status_options
-    case @order.transaction_status
-    when "waiting", "paid_up", "shipped"
-      options_for_transaction_status
-    end
-  end
-
-  def options_for_transaction_status
-    [
-      [I18n.t("enums.order.transaction_status.pending"), "waiting"],
-      [I18n.t("enums.order.transaction_status.paid_up"), "paid_up"],
-      [I18n.t("enums.order.transaction_status.shipped"), "shipped"]
-    ]
-  end
-
   private
 
   def order_params
--- app/helpers/admin/order_details_helper.rb
+++ app/helpers/admin/order_details_helper.rb
@@ -4,4 +4,19 @@ module Admin::OrderDetailsHelper
     subtotal = order_detail.price_including_tax * order_detail.amount
     subtotal
   end
+
+  def transaction_status_options(order)
+    case order.transaction_status
+    when "waiting", "paid_up", "shipped"
+      options_for_transaction_status
+    end
+  end
+
+  def options_for_transaction_status
+    [
+      [I18n.t("enums.order.transaction_status.pending"), "waiting"],
+      [I18n.t("enums.order.transaction_status.paid_up"), "paid_up"],
+      [I18n.t("enums.order.transaction_status.shipped"), "shipped"]
+    ]
+  end
 end

dca13e0084cfacdb61b3c905efd5d1abb020e263
mygw5 <135333208+mygw5@users.noreply.github.com>
2023-08-21 05:37:00 +0000

    Fix top

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -4,7 +4,7 @@ class Public::SearchesController < ApplicationController
     @genres = Genre.all
     @word = params[:word]
     @search_items = Item.where("name LIKE ?", "%#{@word}%")
-    @items = @search_items.page(params[:page]).per(8)
+    @items = @search_items.page(params[:page])
   end
 
 end

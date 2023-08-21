0e7ec90a521bdc8da8ac25968555f8e85cff239f
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-08-21 02:59:51 +0000

    Fix テスト箇所修正

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,5 +2,5 @@ class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
 
-  validates :amount, presence:true
+  validates :amount, presence:true,numericality: { greater_than_or_equal_to: 1, less_than: 100 }
 end

af0c631ff5e60938d21b005b975a601e4ae7d3b9
mygw5 <135333208+mygw5@users.noreply.github.com>
2023-08-22 02:28:44 +0000

    Add　validates

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :customer
   validates :name, presence:true
-  validates :postal_code, presence:true
+  validates :postal_code, presence:true, numericality: {only_integer: true}
   validates :address, presence:true
 
   def address_display
